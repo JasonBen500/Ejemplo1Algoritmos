513328a40ac35dcdff8e8019b3a9a1b23f32aafd
alumno <alumno@10.0.0.30>
2025-08-02 17:03:54 -0600

    "cambios 02-08-2025"

---
+++ src/main/java/GITHUB1.java
@@ -0,0 +1,23 @@
+
+import static java.time.Clock.system;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alumno
+ */
+public class GITHUB1 {
+    
+}
+public static void main(String[] args){
+string nombre, edad;
+Scanner lector=new Scanner(system.in);
+System.out.println("Ingrese su nombre");
+nombre=lector.next();
+System.out.println("Ingrse su edad");
+edad=lector.next();
+}
---
+++ src/main/java/com/mycompany/github/Github.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.github;
+
+/**
+ *
+ * @author alumno
+ */
+public class Github {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        string nombre;
+        scanner.lector=new Scanner(System.in)
+    }
+}
